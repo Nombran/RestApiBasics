5f442afdd0962b0142ee81befbca5c38baf3ad17
Artsiom Berastsen <Artsiom_berastsen@epam.com>
2020-06-07 20:02:45 +0300

    Add WebConfig class with Web Mvc configuration

---
+++ web/src/main/java/com/epam/esm/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.epam.esm.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.epam.esm.**")
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.defaultContentType(MediaType.APPLICATION_JSON);
+    }
+}
