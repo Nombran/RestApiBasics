11aaa1935af4f36ec22f0caf4071d0270936087a
Artsiom Berastsen <Artsiom_berastsen@epam.com>
2020-06-09 19:38:59 +0300

    Add validation annotations for certificate model

--- repository/src/main/java/com/epam/esm/model/Certificate.java
+++ repository/src/main/java/com/epam/esm/model/Certificate.java
@@ -1,10 +1,17 @@
 package com.epam.esm.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -14,15 +21,22 @@ public class Certificate {
 
     private long id;
 
+    @NotBlank
     private String name;
 
+    @NotBlank
     private String description;
 
+    @DecimalMin(value = "0.0")
+    @NotNull
     private BigDecimal price;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime creationDate;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime modificationDate;
 
+    @Min(1)
     private int duration;
 }
--- web/src/main/java/com/epam/esm/config/SpringConfig.java
+++ web/src/main/java/com/epam/esm/config/SpringConfig.java
@@ -1,12 +1,18 @@
 package com.epam.esm.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @Configuration
 @ComponentScan({ "com.epam.esm.**" })
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 public class SpringConfig {
 
+    @Bean
+    public javax.validation.Validator localValidatorFactoryBean() {
+        return new LocalValidatorFactoryBean();
+    }
 }
