4394ef263bdf2e1df46e716c16a80dde3cba0c55
Artsiom Berastsen <Artsiom_berastsen@epam.com>
2020-06-09 20:20:18 +0300

    Add certificate service interface and it's implementation

---
+++ service/src/main/java/com/epam/esm/certificate/CertificateService.java
@@ -0,0 +1,8 @@
+package com.epam.esm.certificate;
+
+import com.epam.esm.CrudService;
+import com.epam.esm.model.Certificate;
+
+public interface CertificateService extends CrudService<Certificate> {
+
+}
